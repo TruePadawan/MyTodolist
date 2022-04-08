041c41ec9f51ff338ba3ab18d32806722a5b6102
Hermes <hermeschigoziri@gmail.com>
2022-04-08 22:05:42 +0100

    add more functionality
    Add functionality for updating and deleting todo item in firebase database

--- src/functions/firebase_db.js
+++ src/functions/firebase_db.js
@@ -1,7 +1,7 @@
-import { ref, push } from "firebase/database";
+import { ref, push, remove, update } from "firebase/database";
 import { database } from "../firebase/firebase_init";
 
-export function storeTodoItemInDB(userID, todoItem) {
+export function addTodoItem(userID, todoItem) {
   const todoItemsDBRef = ref(database, `/${userID}/items`);
   let success;
   let key = '';
@@ -17,4 +17,24 @@ export function storeTodoItemInDB(userID, todoItem) {
   }
 
   return { success,key };
+}
+
+export function updateTodoItem(userID, itemID, value) {
+  try {
+    const itemRef = ref(database, `${userID}/items/${itemID}`);
+    update(itemRef, value);
+  }
+  catch (error) {
+    alert(error.message);
+  }
+}
+
+export function deleteTodoItem(userID, itemID) {
+  try {
+    const itemRef = ref(database, `${userID}/items/${itemID}`);
+    remove(itemRef);
+  }
+  catch (error) {
+    alert(error.message);
+  }
 }
