bb81412d6fb988b6abb483f9bf50f93f07578724
Hermes <hermeschigoziri@gmail.com>
2022-04-08 22:10:08 +0100

    Make use of the updating/deleting functionality
    Make use of the updating/deleting functionality that was added

--- src/components/todolist-body/EditTodoItem/EditTodoItemDialog.js
+++ src/components/todolist-body/EditTodoItem/EditTodoItemDialog.js
@@ -1,6 +1,7 @@
 import { useContext, useState } from "react";
 import TodoListContext from "../../context/TodoListContext";
 import Modal from "../../modal/Modal";
+
 import DoneIcon from '@mui/icons-material/Done';
 import SaveIcon from '@mui/icons-material/Save';
 import DeleteIcon from "@mui/icons-material/Delete";
@@ -10,20 +11,81 @@ import styles from "./EditTodoItemDialog.module.css";
 const EditTodoItemDialog = (props) => {
   const [editItemInputValue, setEditItemInputValue] = useState(props.currentValue);
 
-  const { contextData, setContextData } = useContext(TodoListContext);
+  const { contextData, setContextData, MainController } = useContext(TodoListContext);
 
   function updateTodoListItem(e) {
     e.preventDefault();
-    const updatedItemTitle = document.getElementById("editItemInput").value;
-    const itemIndex = contextData.taskList.findIndex((item) => item.id === props.itemID);
-    let currentItem = contextData.taskList[itemIndex];
 
-    /* Update context value only if the new value isn't the same as the already-stored title for task item to be updated */
-    if (currentItem.title.trim() !== updatedItemTitle.trim()) {
+    const updatedItemTitle = document.getElementById("editItemInput").value;
+    
+    if (!MainController.userLoggedIn)
+    {
+      const itemIndex = contextData.taskList.findIndex((item) => item.id === props.itemID);
+      let currentItem = contextData.taskList[itemIndex];
+      
+      /* Update context value only if the new value isn't the same as the already-stored title for task item to be updated */
+      if (currentItem.title.trim() !== updatedItemTitle.trim()) {
+        setContextData((currentContextData) => {
+          if (itemIndex !== -1) {
+            currentContextData.taskList[itemIndex].title = updatedItemTitle;
+            const updatedContextValue = { taskList: currentContextData.taskList };
+            window.localStorage.setObj("taskList", updatedContextValue.taskList);
+
+            return updatedContextValue;
+          }
+          return currentContextData;
+        });
+      }
+    }
+    else {
+      MainController.updateTodoItemInDB(props.itemID,{title: updatedItemTitle});
+    }
+    
+    props.closeDialog();
+  }
+
+  function setTaskDoneOrNotDone(e) {
+    e.preventDefault();
+    
+    if (!MainController.userLoggedIn)
+    {
       setContextData((currentContextData) => {
+        const itemIndex = currentContextData.taskList.findIndex(
+          (item) => item.id === props.itemID
+        );
+
         if (itemIndex !== -1) {
-          currentContextData.taskList[itemIndex].title = updatedItemTitle;
+          const itemCurrentStatus = currentContextData.taskList[itemIndex].complete;
+          currentContextData.taskList[itemIndex].complete = !itemCurrentStatus;
+          let updatedContextValue = { taskList: currentContextData.taskList };
+          window.localStorage.setObj("taskList", currentContextData.taskList);
+
+          return updatedContextValue;
+        }
+        return currentContextData;
+      });
+    }
+    else {
+      MainController.updateTodoItemInDB(props.itemID, {
+        complete: !props.isTaskComplete
+      });
+    }
+    props.closeDialog();
+  }
+
+  function deleteTodoListItem(e) {
+    e.preventDefault();
+
+    if (!MainController.userLoggedIn)
+    {
+      setContextData((currentContextData) => {
+        const itemIndex = currentContextData.taskList.findIndex((item) => item.id === props.itemID);
+        
+        if (itemIndex !== -1) {
+          currentContextData.taskList.splice(itemIndex, 1);
+
           const updatedContextValue = { taskList: currentContextData.taskList };
+
           window.localStorage.setObj("taskList", updatedContextValue.taskList);
 
           return updatedContextValue;
@@ -31,49 +93,14 @@ const EditTodoItemDialog = (props) => {
         return currentContextData;
       });
     }
+    else {
+      MainController.deleteTodoItemFromDB(props.itemID);
+    }
+
     props.closeDialog();
   }
 
-  function setTaskDoneOrNotDone(e) {
-    e.preventDefault();
-    setContextData((currentContextData) => {
-      const itemIndex = currentContextData.taskList.findIndex(
-        (item) => item.id === props.itemID
-      );
 
-      if (itemIndex !== -1) {
-        const itemCurrentStatus = currentContextData.taskList[itemIndex].complete;
-        currentContextData.taskList[itemIndex].complete = !itemCurrentStatus;
-        let updatedContextValue = { taskList: currentContextData.taskList };
-        window.localStorage.setObj("taskList", currentContextData.taskList);
-
-        return updatedContextValue;
-      }
-      return currentContextData;
-    });
-    props.closeDialog();
-  }
-
-  /* Delete task item from list of tasks and update contextData state along with localStorage */
-  function deleteTodoListItem(e) {
-    e.preventDefault();
-
-    setContextData((currentContextData) => {
-      const itemIndex = currentContextData.taskList.findIndex((item) => item.id === props.itemID);
-      
-      if (itemIndex !== -1) {
-        currentContextData.taskList.splice(itemIndex, 1);
-
-        const updatedContextValue = { taskList: currentContextData.taskList };
-
-        window.localStorage.setObj("taskList", updatedContextValue.taskList);
-
-        return updatedContextValue;
-      }
-      return currentContextData;
-    });
-    props.closeDialog();
-  }
 
   return (
     <Modal close={props.closeDialog} className={styles["editItem"]}>
