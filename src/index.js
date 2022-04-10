7c762b1257bde24bf4b383c67b7d8b60fecc0243
Hermes <hermeschigoziri@gmail.com>
2022-04-10 18:26:08 +0100

    minor changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 
 import TodoListContextProvider from "./components/context/TodoListContextProvider";
 import App from "./App";
+
+import "./index.css";
 
 /* Simple functions for easily adding objects and array to localStorage */
 Storage.prototype.setObj = function (key, obj) {
