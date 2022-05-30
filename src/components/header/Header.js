5c268948a1ad6b58a61d9b40a24c01c44c135d50
Hermes <hermeschigoziri@gmail.com>
2022-05-30 21:06:51 +0100

    add Header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,25 @@
 import styles from "./Header.module.css";
 import MenuBtnImg from "./resources/menu.png";
+import { useContext } from "react";
+import TodoListContext from "../context/TodoListContext";
 
 const Header = () => {
+  const { setSideBarState } = useContext(TodoListContext);
+
+  function toggleSidebar() {
+    setSideBarState((currentState) => {
+      return currentState === "closed" ? "opened" : "closed";
+    });
+  }
+
   return (
     <header>
       <nav>
-        <button type="button" className={styles["sidebarBtn"]}>
+        <button
+          type="button"
+          className={styles["sidebarBtn"]}
+          onClick={toggleSidebar}
+        >
           <img src={MenuBtnImg} alt="Sidebar" />
         </button>
         <div className={styles["navMain"]}>
