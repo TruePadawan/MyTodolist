5e33bb6022b9bfec6ac074605b723c35d330a0c3
Hermes <hermeschigoziri@gmail.com>
2022-05-30 22:04:17 +0100

    persist projects-list data when it changes

--- src/components/context/TodoListContextProvider.js
+++ src/components/context/TodoListContextProvider.js
@@ -1,11 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TodoListContext from "./TodoListContext";
+import { persistProjectsListData } from "../../functions/projects";
 import { MainController } from "../../controller/controller";
 
 const TodoListContextProvider = (props) => {
   const [contextData, setContextData] = useState(window.localStorage.getObj("projects"));
   const [userSignedIn, setUserSignedIn] = useState(false);
   const [sidebarState, setSideBarState] = useState("opened");
+
+  useEffect(() => {
+    persistProjectsListData(contextData, userSignedIn);
+  }, [contextData]);
 
   return (
     <TodoListContext.Provider
