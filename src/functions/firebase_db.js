f75cd76614e75e95a7efe93c190186bfd9ea691e
Hermes <hermeschigoziri@gmail.com>
2022-05-31 14:50:28 +0100

    Give the firebase db functions a namespace

--- src/functions/firebase_db.js
+++ src/functions/firebase_db.js
@@ -1,50 +1,52 @@
 import { ref, push, remove, update } from "firebase/database";
 import { database } from "../firebase/firebase_init";
 
-export function addTodoItem(userID, todoItem) {
-  const todoItemsDBRef = ref(database, `/${userID}/items`);
+export const DB_actions = {
+  addTodoItem: function (userID, todoItem) {
+    const todoItemsDBRef = ref(database, `/${userID}/items`);
 
-  try {
-    push(todoItemsDBRef, todoItem);
-  } catch (error) {
-    alert(error.message);
-  }
-}
+    try {
+      push(todoItemsDBRef, todoItem);
+    } catch (error) {
+      alert(error.message);
+    }
+  },
 
-export function updateTodoItem(userID, itemID, value) {
-  const itemRef = ref(database, `${userID}/items/${itemID}`);
+  updateTodoItem: function (userID, itemID, value) {
+    const itemRef = ref(database, `${userID}/items/${itemID}`);
 
-  try {
-    update(itemRef, value);
-  } catch (error) {
-    alert(error.message);
-  }
-}
+    try {
+      update(itemRef, value);
+    } catch (error) {
+      alert(error.message);
+    }
+  },
 
-export function deleteTodoItem(userID, itemID) {
-  const itemRef = ref(database, `${userID}/items/${itemID}`);
+  deleteTodoItem: function (userID, itemID) {
+    const itemRef = ref(database, `${userID}/items/${itemID}`);
 
-  try {
-    remove(itemRef);
-  } catch (error) {
-    alert(error.message);
-  }
-}
+    try {
+      remove(itemRef);
+    } catch (error) {
+      alert(error.message);
+    }
+  },
 
-export function getTodoItemsFromDB(snapshot) {
-  const todoData = snapshot.val();
-  const todoList = [];
+  getTodoItemsFromDB: function (snapshot) {
+    const todoData = snapshot.val();
+    const todoList = [];
 
-  for (let id in todoData) {
-    let todoItem = {
-      [id]: {
-        title: todoData[id].title,
-        complete: todoData[id].complete,
-      },
-    };
+    for (let id in todoData) {
+      let todoItem = {
+        [id]: {
+          title: todoData[id].title,
+          complete: todoData[id].complete,
+        },
+      };
 
-    todoList.push(todoItem);
-  }
+      todoList.push(todoItem);
+    }
 
-  return todoList;
-}
+    return todoList;
+  },
+};
