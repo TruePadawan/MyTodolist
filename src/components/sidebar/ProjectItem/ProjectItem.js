8e8a57101f08abb7bab964ab698694b16b555177
Hermes <hermeschigoziri@gmail.com>
2022-05-31 21:46:57 +0100

    Update ProjectItem.js

--- src/components/sidebar/ProjectItem/ProjectItem.js
+++ src/components/sidebar/ProjectItem/ProjectItem.js
@@ -1,20 +1,24 @@
 import { useContext } from "react";
 import TodoListContext from "../../context/TodoListContext";
-import { setActiveProject, persistProjectsListData, setNewActiveProject } from "../../../functions/projects";
+import { persistProjectsListData, setActiveProject, setNewActiveProject } from "../../../functions/projects";
 import TrashImg from "./resources/trash.png";
 import styles from "./ProjectItem.module.css";
 
 const ProjectItem = (props) => {
-  const { setContextData, userSignedIn } = useContext(TodoListContext);
+  const { setContextData, userSignedIn, setActiveProjectID } = useContext(TodoListContext);
 
   let itemClass = `${styles["projectItem"]} ${(props.active === true) ? styles["active"] : ""}`;
+  if (props.active === true)
+  {
+    setActiveProjectID(props.id);
+  }
 
   function makeActive()
   {
     if (props.active === true) return;
     setContextData((projects) => {
-      setActiveProject(props.id,projects);
-      return { ...projects };
+      let updatedProjects = setActiveProject(props.id,projects);
+      return updatedProjects;
     });
   }
 
@@ -22,9 +26,13 @@ const ProjectItem = (props) => {
   {
     setContextData((projects) => {
       projects[props.id].title = e.target.innerText;
-      persistProjectsListData(projects, userSignedIn); 
+      persistProjectsListData(projects, userSignedIn);
       return projects; // DONT REDRAW PAGE WHEN USER CHANGES PROJECT TITLE
     });
+    if (props.active === true)
+    {
+      document.getElementById("currentProjectTitle").textContent = e.target.innerText;
+    }
   }
 
   function deleteItem(e)
@@ -46,6 +54,7 @@ const ProjectItem = (props) => {
         }
         return {};
       });
+      setActiveProjectID(null);
     }
   }
 
@@ -55,8 +64,7 @@ const ProjectItem = (props) => {
         <span className={styles["projectTitle"]} role="textbox"
           contentEditable={true}
           suppressContentEditableWarning={true}
-          onInput={updateProjectTitle}
-          onClick={e => e.stopPropagation()}>
+          onInput={updateProjectTitle}>
           {props.title}
         </span>
         <img
