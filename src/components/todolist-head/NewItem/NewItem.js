f835c97ec95b093905faecff925c4abcdd105809
Hermes <hermeschigoziri@gmail.com>
2022-05-31 21:46:37 +0100

    Update NewItem.js
    rewrite the logic for adding todoitems to storage locations

--- src/components/todolist-head/NewItem/NewItem.js
+++ src/components/todolist-head/NewItem/NewItem.js
@@ -1,58 +1,62 @@
-import { useContext } from "react";
-
-import AddTaskIcon from "@mui/icons-material/AddTask";
+import { useRef, useContext } from "react";
 import TodoListContext from "../../context/TodoListContext";
+import { todoManager } from "../../../managers/todoManager";
+import AddTaskIcon from "@mui/icons-material/AddTask";
 import Modal from "../../modal/Modal";
 
 import styles from "./NewItem.module.css";
 
-
-
-
 const NewItem = (props) => {
-  const { setContextData, MainController } = useContext(TodoListContext);
+  const titleRef = useRef();
+  const dueDateRef = useRef();
+  const descRef = useRef();
+  const { userSignedIn, activeProjectID, setContextData } = useContext(TodoListContext);
 
-  /* Create a new list of existing tasks and update local storage along with context data state */
-  // function addNewTask(e) {
-  //   e.preventDefault();
-
-  //   let taskTitle_trimmed = document.getElementById("newTaskInput").value.trim();
-  //   if (taskTitle_trimmed.length < 2) {
-  //     return;
-  //   }
-
-  //   if (!MainController.userLoggedIn)
-  //   {
-  //     const newTaskItem = MainController.newTaskItem_Local(taskTitle_trimmed);
-
-  //     setContextData((currentContextValue) => {
-  //       let updatedTasksList = [...currentContextValue.taskList, newTaskItem];
-  //       let updatedContextData = { taskList: updatedTasksList };
-
-  //       window.localStorage.setObj("taskList", updatedContextData.taskList);
-  //       return updatedContextData;
-  //     });
-  //   }
-  //   else {
-  //     MainController.newTaskItem_Database(taskTitle_trimmed);
-  //   }
-    
-  //   props.closeDialog();
-  // }
-  function addTodo(e)
-  {
+  function addTodo(e) {
     e.preventDefault();
+    const title = titleRef.current.value;
+    const dueDate = dueDateRef.current.value;
+    const desc = descRef.current.value;
+    const done = false;
 
-    const title = document.getElementById("title").value.trim();
+    const itemData = {
+      title,
+      dueDate,
+      desc,
+      done,
+    };
+
+    if (!userSignedIn) {
+      const item = todoManager.addTodoItem(false, itemData);
+      const itemID = item.id;
+
+      setContextData((projects) => {
+        try {
+          if (activeProjectID === null) throw new Error("No Projects");
+          if (activeProjectID in projects) {
+            projects[activeProjectID].todos[itemID] = item;
+            return { ...projects };
+          }
+          throw new Error(`Project with ID ${activeProjectID} not found`);
+        }
+        catch (error) {
+          alert(error);
+        }
+      });
+    }
+    else {
+      todoManager.addTodoItem(true, itemData);
+    }
+
+    props.closeDialog();
   }
-
 
   return (
     <Modal className={styles["addTodo"]} close={props.closeDialog}>
       <h3>New Item</h3>
       <form onSubmit={addTodo}>
         <input
-          id="title"
+          ref={titleRef}
           minLength="2"
           maxLength="100"
           placeholder="Review calculus in Math"
@@ -60,12 +64,13 @@ const NewItem = (props) => {
           autoComplete="off"
           required
         />
-        <input type="date" required className="dueDate" />
+        <input type="date" required ref={dueDateRef} />
         <textarea
+          ref={descRef}
           placeholder="Review calculus in math before saturday"
           required
           className={styles["desc"]}
-          minlength="2"
+          minLength="2"
         ></textarea>
         <button className={styles["addTodoBtn"]}>
           <AddTaskIcon /> Add
