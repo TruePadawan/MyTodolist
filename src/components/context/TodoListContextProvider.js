4121f7b6bcf5290c3ddc5f5201939f1951b466f9
Hermes <hermeschigoziri@gmail.com>
2022-05-31 21:44:46 +0100

    Update TodoListContextProvider.js
    Add state for keeping the id of the currently active project

--- src/components/context/TodoListContextProvider.js
+++ src/components/context/TodoListContextProvider.js
@@ -1,13 +1,13 @@
 import { useState, useEffect } from "react";
 import TodoListContext from "./TodoListContext";
-import { persistProjectsListData } from "../../functions/projects";
+import { persistProjectsListData, getActiveProjectID } from "../../functions/projects";
 import { MainController } from "../../controller/controller";
 
 const TodoListContextProvider = (props) => {
   const [contextData, setContextData] = useState(window.localStorage.getObj("projects"));
   const [userSignedIn, setUserSignedIn] = useState(false);
   const [sidebarState, setSideBarState] = useState("opened");
-  const [activeProjectID, setActiveProjectID] = useState("");
+  const [activeProjectID, setActiveProjectID] = useState(getActiveProjectID(contextData));
 
   useEffect(() => {
     persistProjectsListData(contextData);
