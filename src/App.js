4989cd32be401d19ba9acee4a784bc86921bf4f0
Hermes <hermeschigoziri@gmail.com>
2022-06-01 21:38:06 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,45 +15,51 @@ import TodoListBody from "./components/todolist-body/TodoListBody";
 import "./App.css";
 
 function App() {
-  const {
-    sidebarState,
-    projects,
-    setProjects,
-    activeProjectID,
-    userSignedIn, setUserSignedIn,
-    userID, setUserID,
-  } = useContext(TodoListContext);
+  const { sidebarState, projects, setProjects } = useContext(TodoListContext);
   const signedInIndicatorRef = useRef(null);
   const [user, setUser] = useState("Sign In");
 
   const loadAppData = useCallback((snapshot) => {
     const data = DB_actions.getAppData(snapshot);
-
-    console.log('appdata', data);
     if (data === null)
     {
+      // IF USER IS SIGNING IN FOR FIRST TIME, CREATE DEFAULT PROJECT
       appManager.addProjectItem(true, {
         title : "Default",
         active : true,
-        todos : {}
-      }, userID);
+        todos : {},
+      });
     }
-  }, [userID]);
+    else {
+      for (const id in data)
+      {
+        if (data[id].active === true)
+        {
+          appManager.setActiveProjectID(id);
+        }
+        if (!data[id].hasOwnProperty("todos"))
+        {
+          data[id].todos = {};
+        }
+      }
+      setProjects(data);
+    }
+  }, [setProjects]);
 
   /* When app loads, sign in the user if there was a previous sign in */
   useEffect(() => {
     auth.onAuthStateChanged(() => {
-      if (auth.currentUser && !userSignedIn)
+      if (auth.currentUser && !appManager.userSignedIn)
       {
         setUser(auth.currentUser.displayName);
-        setUserSignedIn(true);
-        setUserID(auth.currentUser.uid);
+        appManager.setUserSignedIn(true);
+        appManager.setUID(auth.currentUser.uid);
 
         const projectsDataRef = ref(database, `/${auth.currentUser.uid}/projects`);
         onValue(projectsDataRef, loadAppData);
       }
     });
-  }, [loadAppData, setUserID, setUserSignedIn, userSignedIn]);
+  }, [loadAppData]);
 
   const signInWithGoogle = async () => {
     // IF THERE IS NO USER CURRENTLY LOGGED IN, LOGIN. ELSE, SIGN OUT
@@ -70,14 +76,14 @@ function App() {
       await signOut(auth);
 
       setUser("Sign In");
-      setUserSignedIn(false);
-      setUserID("");
+      appManager.setUserSignedIn(false);
+      appManager.setUID("");
 
       setProjects(window.localStorage.getObj("projects")); // WHEN THE USER SIGNS OUT, LOAD TODOITEMS FROM LOCAL STORAGE
     }
   };
 
-  const mainClassName = sidebarState === "closed" ? "sidebarClosed" : "sidebarOpened";
+  let activeProjectID = appManager.activeProjectID;
   let activeProject = projects[activeProjectID];
   // IN CASE OF NO PROJECT ITEMS
   if (activeProjectID === null)
@@ -87,7 +93,8 @@ function App() {
       todos : {}
     }
   }
-
+  
+  const mainClassName = sidebarState === "closed" ? "sidebarClosed" : "sidebarOpened";
   return (
     <div className="App">
       <Header />
