04de846d432386bff2017ad66c31a9899978464b
Hermes <hermeschigoziri@gmail.com>
2022-06-02 19:11:12 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { onValue, ref } from "firebase/database";
 import { auth, database, googleProvider } from "./firebase/firebase_init";
 import { DB_actions } from "./functions/firebase_db";
 import { appManager } from "./managers/appManager";
+import { getActiveProject, getLocalAppData, toLocalStorage } from "./functions/projects";
+import { v4 as uuidv4 } from "uuid";
 import Button from "@mui/material/Button";
 import GoogleIcon from "@mui/icons-material/Google";
 import Header from "./components/header/Header";
@@ -11,31 +13,35 @@ import SideBar from "./components/sidebar/sidebar";
 import TodoListContext from "./components/context/TodoListContext";
 import TodoListHeader from "./components/todolist-head/TodoListHeader";
 import TodoListBody from "./components/todolist-body/TodoListBody";
+import Info from "./components/info/info";
 
 import "./App.css";
 
 function App() {
   const { sidebarState, projects, setProjects } = useContext(TodoListContext);
   const signedInIndicatorRef = useRef(null);
+  const [onLoading, setLoading] = useState(true);
   const [user, setUser] = useState("Sign In");
 
-  const loadAppData = useCallback((snapshot) => {
-    const data = DB_actions.getAppData(snapshot);
-    if (data === null)
-    {
+  const loadAppData = useCallback(
+    (snapshot) => {
+      const data = DB_actions.getAppData(snapshot);
+      console.log('data from db', data);
+      setLoading(false);
+
       // IF USER IS SIGNING IN FOR FIRST TIME, CREATE DEFAULT PROJECT
-      appManager.addProjectItem(true, {
-        title : "Default",
-        active : true,
-        todos : {},
-      });
-    }
-    else {
+      if (data === null)
+      {
+        const item = appManager.createProjectItem("Default", true);
+        DB_actions.addProjectItem(appManager.uid, item);
+        return;
+      }
+      // ELSE LOAD APPDATA
       for (const id in data)
       {
         if (data[id].active === true)
         {
-          appManager.setActiveProjectID(id);
+          appManager.activeProjectID = id;
         }
         if (!data[id].hasOwnProperty("todos"))
         {
@@ -43,80 +49,103 @@ function App() {
         }
       }
       setProjects(data);
-    }
-  }, [setProjects]);
+    }, [setLoading, setProjects]);
 
   /* When app loads, sign in the user if there was a previous sign in */
   useEffect(() => {
-    auth.onAuthStateChanged(() => {
-      if (auth.currentUser && !appManager.userSignedIn)
-      {
+    auth.onAuthStateChanged(function signUserIn() {
+      if (auth.currentUser && !appManager.userSignedIn) {
+        setLoading(true);
         setUser(auth.currentUser.displayName);
-        appManager.setUserSignedIn(true);
-        appManager.setUID(auth.currentUser.uid);
+        appManager.userSignedIn = true;
+        appManager.uid = auth.currentUser.uid;
 
-        const projectsDataRef = ref(database, `/${auth.currentUser.uid}/projects`);
-        onValue(projectsDataRef, loadAppData);
+        const appDataRef = ref(
+          database,
+          `/${auth.currentUser.uid}/projects`
+        );
+        onValue(appDataRef, loadAppData);
       }
     });
   }, [loadAppData]);
 
   const signInWithGoogle = async () => {
     // IF THERE IS NO USER CURRENTLY LOGGED IN, LOGIN. ELSE, SIGN OUT
-    if (auth.currentUser === null)
-    {
-      try {
-        await signInWithPopup(auth, googleProvider);
-      } catch (error) {
-        alert(error);
+    try {
+      if (auth.currentUser === null) {
+          await signInWithPopup(auth, googleProvider);
       }
-    }
-    else
+      else {
+        await signOut(auth);
+  
+        setUser("Sign In");
+        appManager.userSignedIn = false;
+        appManager.uid = "";
+  
+        setProjects(getLocalAppData()); // WHEN THE USER SIGNS OUT, LOAD TODOITEMS FROM LOCAL STORAGE
+      }
+    } catch (error)
     {
-      await signOut(auth);
-
-      setUser("Sign In");
-      appManager.setUserSignedIn(false);
-      appManager.setUID("");
-
-      setProjects(window.localStorage.getObj("projects")); // WHEN THE USER SIGNS OUT, LOAD TODOITEMS FROM LOCAL STORAGE
+      alert(error.message);
     }
   };
 
-  let activeProjectID = appManager.activeProjectID;
-  let activeProject = projects[activeProjectID];
-  // IN CASE OF NO PROJECT ITEMS
-  if (activeProjectID === null)
-  {
-    activeProject = {
-      title : "",
-      todos : {}
-    }
-  }
-  
+  useEffect(() => {
+    setTimeout(() => {
+      if (auth.currentUser === null) {
+        setLoading(false);
+
+        // CREATE DEFAULT PROJECT IF THERE IS NONE, ELSE LOAD EXISTING APP DATA
+        const appData = getLocalAppData();
+        const amountOfProjects = Object.keys(appData).length;
+        if (amountOfProjects === 0)
+        {
+          const itemID = uuidv4();
+          const item = appManager.createProjectItem("Default", true);
+          const projects = {
+            [itemID] : item
+          };
+
+          toLocalStorage(projects);
+          setProjects(projects);
+          return;
+        }
+        setProjects(appData);
+      }
+    }, 4000);
+  }, [setProjects]);
+
+  const activeProject = getActiveProject(projects, appManager.activeProjectID);
   const mainClassName = sidebarState === "closed" ? "sidebarClosed" : "sidebarOpened";
+
   return (
-    <div className="App">
-      <Header />
-      <main className={mainClassName}>
-        <SideBar state={sidebarState}>
-          <Button
-          id="signInBtn"
-          variant="outlined"
-          startIcon={<GoogleIcon />}
-          onClick={signInWithGoogle}>
-            <span ref={signedInIndicatorRef}>{user}</span>
-          </Button>
-        </SideBar>
-        <section className="container" aria-labelledby="currentProjectTitle">
-          <h2 id="currentProjectTitle">{activeProject.title}</h2>
-          <div className="todolist">
-            <TodoListHeader />
-            <TodoListBody todos={activeProject.todos}/>
-          </div>
-        </section>
-      </main>
-    </div>
+    <>
+    {onLoading && (
+      <Info message={"Loading AppData..."} />
+    )}
+      <div className="App">
+        <Header />
+        <main className={mainClassName}>
+          <SideBar state={sidebarState} data={projects}>
+            <Button
+              id="signInBtn"
+              variant="outlined"
+              startIcon={<GoogleIcon />}
+              onClick={signInWithGoogle}
+            >
+              <span ref={signedInIndicatorRef}>{user}</span>
+            </Button>
+          </SideBar>
+          <section className="container" aria-labelledby="currentProjectTitle">
+            <h2 id="currentProjectTitle">{activeProject.title}</h2>
+            <div className="todolist">
+              <TodoListHeader />
+              <TodoListBody todos={activeProject.todos} />
+            </div>
+          </section>
+        </main>
+      </div>
+    </>
   );
 }
 
