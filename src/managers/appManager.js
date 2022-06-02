e01e1c5467b3e341843f447b2d290cd9a7232510
Hermes <hermeschigoziri@gmail.com>
2022-06-02 19:10:16 +0100

    Update appManager.js

--- src/managers/appManager.js
+++ src/managers/appManager.js
@@ -1,66 +1,17 @@
-import { v4 as uuidv4 } from "uuid";
-import { createProjectItem } from "../functions/projects";
-import { DB_actions } from "../functions/firebase_db";
-
 class AppManager {
-    uid = null;
-    activeProjectID = null;
-    userSignedIn = false;
+  uid = null;
+  activeProjectID = null;
+  userSignedIn = false;
 
-    setUID(id)
-    {
-        this.uid = id;
-    }
-    
-    setUserSignedIn(value)
-    {
-        this.userSignedIn = value;
-    }
+  createProjectItem(title, active = false) {
+    let projectItem = {
+      title,
+      active,
+      todos: {},
+    };
 
-    setActiveProjectID(id)
-    {
-        this.activeProjectID = id;
-    }
-
-    #todoItemLocal(data)
-    {
-        const ID = uuidv4();
-        data.id = ID;
-        return data;
-    }
-
-    #todoItemDB(data, projectID)
-    {
-        DB_actions.addTodoItem(this.uid, projectID, data);
-    }
-
-    #projectItemLocal(title)
-    {
-        return createProjectItem(title);
-    }
-
-    #projectItemDB(userID, data)
-    {
-        DB_actions.addProjectItem(userID, data);
-    }
-
-    addTodoItem(isUserSignedIn, data, projectID = "")
-    {
-        if (!isUserSignedIn)
-        {
-            return this.#todoItemLocal(data);
-        }
-        this.#todoItemDB(data, projectID);
-    }
-
-    addProjectItem(isUserSignedIn, data)
-    {
-        if (!isUserSignedIn)
-        {
-            return this.#projectItemLocal(data.title);
-        }
-        this.#projectItemDB(this.uid, data);
-    }
+    return projectItem;
+  }
 }
 
 export const appManager = new AppManager();
