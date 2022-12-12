7a1e12bcebf96fe9e9ced2651ab1c8c33a66aee2
Hermes <hermeschigoziri@gmail.com>
2022-12-12 15:39:34 +0100

    switch to using new method of rendering

--- src/index.js
+++ src/index.js
@@ -1,26 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import AuthContextProvider from "./context/AuthContextProvider";
 import App from "./App";
 import TodoListContextProvider from "./context/TodoListContextProvider";
 import "./index.css";
 
-/* Simple functions for easily adding objects and array to localStorage */
-Storage.prototype.setObj = function (key, obj) {
-	return this.setItem(key, JSON.stringify(obj));
-};
-
-Storage.prototype.getObj = function (key) {
-	return JSON.parse(this.getItem(key));
-};
-
-ReactDOM.render(
-	<React.StrictMode>
+const root = createRoot(document.getElementById("root"));
+root.render(
+	<StrictMode>
 		<AuthContextProvider>
 			<TodoListContextProvider>
 				<App />
 			</TodoListContextProvider>
 		</AuthContextProvider>
-	</React.StrictMode>,
-	document.getElementById("root")
+	</StrictMode>
 );
