32e119983ecef700926c232ed06ef9712622a533
Hermes <hermeschigoziri@gmail.com>
2022-12-15 13:52:09 +0100

    remove unused state

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import Sidebar from "./components/layout/sidebar/Sidebar";
 import { TodoListContext } from "./context/TodoListContextProvider";
 import TodoListHead from "./components/layout/todolist-head/TodoListHead";
 import TodoListBody from "./components/layout/todolist-body/TodoListBody";
-import Info from "./components/other/info/info";
 import GoogleAuthBtn from "./components/other/button/GoogleAuthBtn/GoogleAuthBtn";
 import { getActiveProject } from "./utils/other-utils";
 import "./App.css";
 
 export default function App() {
-	const [loading, setLoading] = useState(false);
 	const [sidebarIsOpen, setSidebarIsOpen] = useState(true);
 	const { data } = useContext(TodoListContext);
 
@@ -26,36 +24,27 @@ export default function App() {
 
 	return (
 		<Fragment>
-			<Info
-				message={"Retrieving data from database..."}
-				open={loading}
-				onClose={() => false}
-			/>
-			<div className="App">
-				<Header onSidebarBtnClicked={toggleSidebar} />
-				<main className={mainClassName}>
-					<Sidebar data={data} open={sidebarIsOpen}>
-						<GoogleAuthBtn />
-					</Sidebar>
-					<section
-						className="container"
-						aria-labelledby="current-project-title">
-						<h2 id="current-project-title" className="current-project-title">
-							{activeProjectData.title}
-						</h2>
-						<div className="todolist">
-							<TodoListHead
-								appData={data}
-								activeProjectData={activeProjectData}
-							/>
-							<TodoListBody
-								projectID={activeProjectData.id}
-								todos={activeProjectData.todos}
-							/>
-						</div>
-					</section>
-				</main>
-			</div>
+			<Header onSidebarBtnClicked={toggleSidebar} />
+			<main className={mainClassName}>
+				<Sidebar data={data} open={sidebarIsOpen}>
+					<GoogleAuthBtn />
+				</Sidebar>
+				<section className="container" aria-labelledby="current-project-title">
+					<h2 id="current-project-title" className="current-project-title">
+						{activeProjectData.title}
+					</h2>
+					<div className="todolist">
+						<TodoListHead
+							appData={data}
+							activeProjectData={activeProjectData}
+						/>
+						<TodoListBody
+							projectID={activeProjectData.id}
+							todos={activeProjectData.todos}
+						/>
+					</div>
+				</section>
+			</main>
 		</Fragment>
 	);
 }
